904c8f7a7e02290476a2cdd7f6d866a422e5fe52
SteezyyDou <joacomrfernandez@gmail.com>
2024-10-20 16:47:32 -0300

    Login

---
+++ src/controlador.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!empty($_POST["btningresar"])) {
+    if (empty($_POST["nombre"] || empty($_POST["contraseña"]))) {
+        echo '<div>Los campos estan vacios.</div>';
+    } else {
+        $nombre=$_POST["nombre"];
+        $contraseña=$_POST["contraseña"];
+        $sql = $conexion->query(" select * from usuario where nombre='$nombre' and contraseña='$contraseña' ");
+        if ($datos=$sql->fetch_object()){
+            header("location:Bienvenido");
+        } else {
+            echo '<div>Acceso denegado</div>';
+
+        }
+    }
+    
+}
+
+
+?>
